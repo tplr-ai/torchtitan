ba0e170c59cc0ebbe75a70044528115e845a3111
Quentin Anthony <qganthony@yahoo.com>
2025-07-17 13:05:15 -0700

    skip packages we don't need for now

--- torchtitan/experiments/__init__.py
+++ torchtitan/experiments/__init__.py
@@ -4,5 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import torchtitan.experiments.llama4  # noqa: F401
 import torchtitan.experiments.simple_fsdp  # noqa: F401
+
+import importlib, warnings
+
+try:
+    importlib.import_module(f"torchtitan.models.llama4")  # noqa: F401
+except ImportError as e:
+    warnings.warn(f"Skipping llama4: {e}")
--- torchtitan/models/__init__.py
+++ torchtitan/models/__init__.py
@@ -7,6 +7,13 @@
 
 # Import the built-in models here so that the corresponding register_model_spec()
 # will be called.
-import torchtitan.models.deepseek_v3  # noqa: F401
+
 import torchtitan.models.llama3  # noqa: F401
 import torchtitan.models.llama3_ft  # noqa: F401
+
+import importlib, warnings
+
+try:
+    importlib.import_module(f"torchtitan.models.deepseek_v3")  # noqa: F401
+except ImportError as e:
+    warnings.warn(f"Skipping deepseek_v3: {e}")
