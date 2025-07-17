4489819927665a3b3c70dbe686fca5b3c9dcae79
Quentin Anthony <qganthony@yahoo.com>
2025-07-17 13:05:15 -0700

    skip packages we don't need for now

--- torchtitan/models/__init__.py
+++ torchtitan/models/__init__.py
@@ -7,5 +7,12 @@
 
 # Import the built-in models here so that the corresponding register_model_spec()
 # will be called.
-import torchtitan.models.deepseek_v3  # noqa: F401
+
 import torchtitan.models.llama3  # noqa: F401
+
+import importlib, warnings
+
+try:
+    importlib.import_module(f"torchtitan.models.deepseek_v3")  # noqa: F401
+except ImportError as e:
+    warnings.warn(f"Skipping deepseek_v3: {e}")
