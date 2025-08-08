ced02c2d32ca229edebacd1e3f4be59bddc89916
Joel Lidin <joellidin@gmail.com>
2025-08-08 15:57:02 +0200

    Run pre-commit

--- torchtitan/models/__init__.py
+++ torchtitan/models/__init__.py
@@ -8,12 +8,13 @@
 # Import the built-in models here so that the corresponding register_model_spec()
 # will be called.
 
+import importlib
+import warnings
+
 import torchtitan.models.llama3  # noqa: F401
 import torchtitan.models.llama3_ft  # noqa: F401
 
-import importlib, warnings
-
 try:
-    importlib.import_module(f"torchtitan.models.deepseek_v3")  # noqa: F401
+    importlib.import_module("torchtitan.models.deepseek_v3")  # noqa: F401
 except ImportError as e:
     warnings.warn(f"Skipping deepseek_v3: {e}")
